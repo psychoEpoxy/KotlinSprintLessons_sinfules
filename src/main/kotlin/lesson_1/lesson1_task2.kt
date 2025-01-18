689a33a5d9232d9732cbeaa35c962cc78e300fb6
sinful <rechgames@gmail.com>
2025-01-18 13:01:42 +0300

    KS-1-2 complete solution task 2

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,13 @@
+package lesson_1
+
+fun main() {
+    var employeeCount: Int = 2000
+    var numberOfOrders: Int = 75
+    val customerThankYouText: String = "Благодарим Вас за покупку $numberOfOrders позиций товара!"
+    println(numberOfOrders)
+    println(customerThankYouText)
+
+//    println(employeeCount)
+    employeeCount = 1999
+    println(employeeCount)
+}
