a854e186790bcbe438716e8245fb0a03eca3ca2d
sinful <rechgames@gmail.com>
2025-01-19 15:00:14 +0300

    KS-3-2 task is completed

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    var userName = "Андреева Татьяна Сергеевна"
+    val age = 20
+    println("$userName, $age")
+
+    val marriageAge = 22
+    userName = "Сидорова Татьяна Сергеевна"
+    println("$userName, $marriageAge")
+}
