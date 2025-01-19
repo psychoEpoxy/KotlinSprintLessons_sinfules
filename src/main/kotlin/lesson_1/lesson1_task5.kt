b1911b3c11a1fe554345fbecf2aeb496e8471c17
sinful <rechgames@gmail.com>
2025-01-19 13:28:03 +0300

    KS-1-5 added constants, changed the output via string formatting

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,11 +2,15 @@ package lesson_1
 
 fun main() {
     val seconds: Short = 6380
-    val hours = seconds / 3600
-    val remainingSeconds = seconds % 3600
+    val hours = seconds / SECOND_IN_HOUR
+    val remainingSeconds = seconds % SECOND_IN_HOUR
 
-    val minute = remainingSeconds / 60
-    val newSeconds = remainingSeconds % 60
+    val minute = remainingSeconds / SECOND_IN_MINUTE
+    val newSeconds = remainingSeconds % SECOND_IN_MINUTE
 
-    println("$hours:$minute:$newSeconds")
+    val formattedTime = String.format("%02d:%02d:%02d", hours, minute, newSeconds)
+    println(formattedTime)
 }
+
+const val SECOND_IN_HOUR = 3600
+const val SECOND_IN_MINUTE = 60
