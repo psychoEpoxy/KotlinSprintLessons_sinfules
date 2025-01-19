a9afe52c560666e41204d1fd31fe4befa9e21fb5
sinful <rechgames@gmail.com>
2025-01-19 21:31:55 +0300

    KS-2-4: отбросил дробную часть с помощью toInt()

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,13 +1,13 @@
 package lesson_2
 
 fun main() {
-    val buff: Byte = 20
+    val buff = 0.2
 
-    val crystal: Byte = 7
-    val iron: Byte = 11
+    val crystal = 7
+    val iron = 11
 
-    val bonusCrystal = (crystal * buff) / 100
-    val bonusIron = (iron * buff) / 100
+    val bonusCrystal = (crystal * buff).toInt()
+    val bonusIron = (iron * buff).toInt()
 
     println("Бонусные кристалы - $bonusCrystal")
     println("Бонусное железо - $bonusIron")
