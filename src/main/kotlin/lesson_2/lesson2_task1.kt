31a20324c7932e9615711e9386018e7a7acc2aa7
sinful <rechgames@gmail.com>
2025-01-19 21:10:09 +0300

    KS-2-1 removed the magic numbers and gave the variables a meaningful name

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,12 +1,14 @@
 package lesson_2
 
 fun main() {
-    val mark1: Int = 3
-    val mark2: Int = 4
-    val mark3: Int = 3
-    val mark4: Int = 5
+    val numberOfStudents = 4
+    val markFirstStudent = 3
+    val markSecondStudent = 4
+    val markThirdStudent = 3
+    val markFourthStudent = 5
 
-    val averageScore: Float = (mark1 + mark2 + mark3 + mark4) / 4.0f
+    val averageScore =
+        (markFirstStudent + markSecondStudent + markThirdStudent + markFourthStudent).toFloat() / numberOfStudents
 
     println("Средняя оценка в классе: ${String.format("%.2f", averageScore)}")
 }
