e12b97486a13983442819c3148d226a24b131949
sinful <sirus_2003@mail.ru>
2025-01-22 10:37:19 +0300

    KS-2-4: the bonus effect as an integer

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,13 +1,13 @@
 package lesson_2
 
 fun main() {
-    val buff = 0.2
+    val buff = 20
 
     val crystal = 7
     val iron = 11
 
-    val bonusCrystal = (crystal * buff).toInt()
-    val bonusIron = (iron * buff).toInt()
+    val bonusCrystal = (crystal * buff / 100)
+    val bonusIron = (iron * buff / 100)
 
     println("Бонусные кристалы - $bonusCrystal")
     println("Бонусное железо - $bonusIron")
