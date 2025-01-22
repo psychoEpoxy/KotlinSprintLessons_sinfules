4151625db1e5d249a3624b787950bee244409441
sinful <sirus_2003@mail.ru>
2025-01-22 10:57:14 +0300

    KS-2-5: the output and the format of the percentages have been changed

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -3,11 +3,11 @@ package lesson_2
 import kotlin.math.pow
 
 fun main() {
-    val deposit: Double = 70_000.0
-    val percent: Double = 0.167
-    val years: Int = 20
+    val deposit = 70_000.0
+    val percent = 16.7 / 100
+    val years = 20
 
-    val compoundInterest = deposit * (1 + percent).pow(years.toDouble())
+    val compoundInterest = deposit * (1 + percent).pow(years)
 
-    println("Через $years лет под ${percent * 100}% клиет получит: ${String.format("%.3f", compoundInterest)}")
+    println("Размер вклада через $years лет: %.3f".format(compoundInterest))
 }
